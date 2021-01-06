6214eaf41cc78236bdf93b7df61b10b6c96301a4
Renault Pierrick <pierrick.renault@etudiant.univ-rennes1.fr>
2021-01-06 19:39:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Projet_marvel_backend
---
+++ src/test/java/com/experience/marvel/MarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experience.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
